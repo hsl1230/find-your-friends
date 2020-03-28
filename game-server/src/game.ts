fb49a771f1380ef25f2bf7cb2b4bdc23986c97aa
Henry Huang <hslcalgary@gmail.com>
2020-03-28 12:09:16 -0600

    init 3

--- game-server/src/game.ts
+++ game-server/src/game.ts
@@ -1,8 +1,19 @@
 import { Player } from './player';
 import { GamePlayer } from './game-player';
+import { throws } from 'assert';
 
+enum PokerSuits {
+    Spades = 1,
+    Hearts,
+    Clubs,
+    Diamonds
+}
+
+const sortCards = (cards: number[]) => {
+    return cards.sort((card1:number, card2:number) => card2 - card1);
+}
 export class Game {
-    private _masterColor: string | undefined;
+    private _masterSuit: PokerSuits | undefined;
 
     masterGamePlayer: GamePlayer | undefined;
 
@@ -40,14 +51,14 @@ export class Game {
 
     private createGamePlayer(player: Player, position: number) {
         const numberOfPlayers = this.players.length;
-        const poker: number[] = [];
+        const cards: number[] = [];
 
         for (let i = 0; i < this.shuffledPoker.length - this.reservedPoker.length; i++) {
             if (i % numberOfPlayers === position) {
-                poker.push(this.shuffledPoker[i]);
+                cards.push(this.shuffledPoker[i]);
             }
         }
-        return new GamePlayer(player, poker);
+        return new GamePlayer(player, sortCards(cards));
     }
 
     addPoints(points: number) {
@@ -58,12 +69,34 @@ export class Game {
         this.masterGamePlayer = this.gamePlayers.find(gamePlayer => gamePlayer.player.playerId === playerId);
     }
 
-    set masterColor(color: string | undefined) {
-        this._masterColor = color;
+    set masterSuit(suit: PokerSuits | undefined) {
+        this._masterSuit = suit;
     }
 
-    get masterColor(): string | undefined {
-        return this._masterColor;
+    get masterSuit(): PokerSuits | undefined {
+        return this._masterSuit;
+    }
+
+    organizeCards() {
+        if (this.masterSuit) {
+            this.masterSuit * 13
+        }
+        const suitNumber = this.masterSuit? this.masterSuit: 0;
+        const level: number = this.masterGamePlayer? this.masterGamePlayer.player.level : this.players[0].level;
+        
+        if (this.gamePlayers.length === this.players.length) {
+            this.gamePlayers.forEach(gamePlayer => {
+                gamePlayer.cards.map((card: number) => {
+                    if (card > 51) {
+                        return card + 3000;
+                    } else if ((card + 1) % 13 === level - 1) {
+                        return card + 2000;
+                    } else if (card >= suitNumber * 13 && card < (suitNumber +1) * 13) {
+                        return card + 1000;
+                    }
+                });
+            });
+        }
     }
 
     private shuffle() {
